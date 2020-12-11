f139c4e5ff617d4ac3bdabd6d189c55641284315
james lavis <RealHydraElite@gmail.com>
2020-12-11 14:38:12 +0000

    I just added more text the website nothing big yet

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("newest update 12/10/20");
+console.log("next update 12/11/20");
 //this will run when website is loaded.
 function betaweb() {
   var enterright = prompt("enter code for testers. Sorry for putting a code but this will only last until I am done developing the website and fixing the bugs in the code.If you don't know the code for the beta close this tab if you dont it will keep asking for the code. dont click ok or cancel");
@@ -8,9 +9,4 @@ function betaweb() {
   } else {
     window.onload(window.close());
   }
-}
-function math(){
-  var num1=prompt("what is the first number");
-  var num2=prompt("what is the second number");
-  var add= num1+2;
 }
