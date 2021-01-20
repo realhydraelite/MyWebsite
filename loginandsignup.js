f4cbcec7e03773f25b0dd405c6eea211261bcad9
james lavis <RealHydraElite@gmail.com>
2021-01-20 17:28:16 +0000

    updates: i deleted some stuff. a add to the read me file. i add the comments to login and signup file.

--- loginandsignup.js
+++ loginandsignup.js
@@ -1,25 +1,27 @@
-var lse=localStorage.setItem("email",email);
-var lsun=localStorage.setItem("username",username);
-var lsp=localStorage.setItem("password",password);
-var glse=localStorage.getItem("email");
-var glsp=localStorage.getItem("password");
-function signup(){
-var username=prompt("create your username");
-lsun=localStorage.setItem("username",username);
-var email=prompt("what is the email you going to use");
-lse=localStorage.setItem("email",email);
-var password=prompt("create a strong password you remember");
-lsp=localStorage.setItem("password",password);
+var lse = localStorage.setItem("email", email);
+var lsun = localStorage.setItem("username", username);
+var lsp = localStorage.setItem("password", password);
+var glse = localStorage.getItem("email");
+var glsp = localStorage.getItem("password");
+//this is the signup this use the localStorage of the brownser.
+function signup() {
+  var username = prompt("create your username");
+  lsun = localStorage.setItem("username", username);
+  var email = prompt("what is the email you going to use");
+  lse = localStorage.setItem("email", email);
+  var password = prompt("create a strong password you remember");
+  lsp = localStorage.setItem("password", password);
 }
-function login(){
-glse=localStorage.getItem("email");;
-glsp=localStorage.getItem("password");
-var wE=prompt("what is your email");
-var wP=prompt("what is your password");
-if(wE==glse&&wP==glsp){
-  alert("this is correct");
-}else{
-  alert("this is wrong");
-}
+//this is the login it allows the use they put into the signup localStorage to see if there is account that is created.
+function login() {
+  glse = localStorage.getItem("email");;
+  glsp = localStorage.getItem("password");
+  var wE = prompt("what is your email");
+  var wP = prompt("what is your password");
+  if (wE == glse && wP == glsp) {
+    alert("this is correct");
+  } else {
+    alert("this is wrong");
+  }
 }
 //ernwo fo edoc si semaj sival
--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
 owner of code:James A lavis
+copyright:got to james lavis.
+help from:https://www.w3schools.com/
